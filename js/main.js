625a7cc137dca1b8652941ba61925da82f6e67cb
Vadim Suleimanov <s1imus05@gmail.com>
2017-10-29 09:02:09 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+import '../css/bootstrap.css';
+import '../css/styles.scss';
+
+import '../node_modules/jquery/dist/jquery'
+import '../node_modules/bootstrap/dist/js/bootstrap'
+
+((w, $, u) => {
+    $('#carousel').on('click', '.goto', function(e){
+        e.preventDefault();
+        e.stopPropagation();
+
+        const id = $(this).attr('href'),
+            steps = 10,
+            marginTop = +$('.navbar:first').height(),
+            top = +$(id)[0].getBoundingClientRect().top,
+            gotoLength = (top - marginTop + window.scrollY),
+            dx = gotoLength / steps;
+
+        let x = window.scrollY, i = 0;
+        const $r = setInterval(() => {
+            i++;
+            if (i >= steps || (x + dx) > gotoLength) {
+                clearInterval($r);
+                window.scrollTo(0, gotoLength);
+                return;
+            }
+            x += dx;
+            window.scrollTo(0, x);
+        }, 22);
+    });
+})(window, jQuery);
---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const webpack = require('webpack'),
+    ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: {
+        main: './js/main.js'
+    },
+    output: {
+        filename: './dist/[name]-bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    use: 'css-loader'
+                })
+            },
+            {
+                test: /\.scss$/,
+                use:[{
+                    loader: 'style-loader'
+                }, {
+                    loader: 'css-loader'
+                }, {
+                    loader: 'sass-loader'
+                }]
+            },
+            {
+                test: /\.(svg|eot|ttf|woff|woff2)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    name: 'fonts/[hash].[ext]'
+                }
+            },
+            {
+                test: /icons\.(svg)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 1,
+                    name: 'ui/[name].[ext]'
+                }
+            },
+            {
+                test: /\.(png|jpg|gif|ico)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    name: 'img/[hash].[ext]'
+                }
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('./dist/[name]-styles.css'),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        })
+    ]
+};
